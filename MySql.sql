7618b9837c07c578c9ae512d5842f3d16717c185
Syed Maaz <syedmaaz78622@gmail.com>
2025-12-14 14:05:02 +0530

    SQL learning

--- MySql.sql
+++ MySql.sql
@@ -1,3 +1,5 @@
+-- DATABASE & TABLES creation 
+
 CREATE DATABASE IF NOT EXISTS sql_learning;
 USE sql_learning;
 
@@ -12,10 +14,27 @@ CREATE TABLE students (
 
 SHOW COLUMNS FROM students;
 
+DROP TABLE IF EXISTS students;
 
+CREATE TABLE courses (
+    course_id INT PRIMARY KEY AUTO_INCREMENT,
+    course_name VARCHAR(100),
+    fee DECIMAL(8,2)
+);
 
+SHOW COLUMNS FROM courses;
+
+CREATE TABLE enrollments (
+    enroll_id INT PRIMARY KEY AUTO_INCREMENT,
+    student_id INT,
+    course_id INT,
+    enroll_date DATE,
+    FOREIGN KEY (student_id) REFERENCES students(id),
+    FOREIGN KEY (course_id) REFERENCES courses(course_id)
+);
+
+SHOW COLUMNS FROM enrollments;
 
 SHOW TABLES;
-
 
 SHOW DATABASES;
